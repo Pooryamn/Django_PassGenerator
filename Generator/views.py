156460b1e0f48a1c2b2a0a54ffe145e3d4605de5
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-09 22:14:50 +0430

    add new template page for home page

--- Generator/views.py
+++ Generator/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello Poorya')
+    return render(request,'generator/home.html')
+
+def Poorya(request):
+    return HttpResponse('<h1>My name is poorya mohammadi nasab</h1>')
