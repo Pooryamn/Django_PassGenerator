f03216994655174d1da1475057a596d4cfae6069
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-09 22:20:34 +0430

    Passing an argument to templates file

--- Generator/views.py
+++ Generator/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,'generator/home.html')
+    return render(request,'generator/home.html',{'password':'9621160025'})
 
 def Poorya(request):
     return HttpResponse('<h1>My name is poorya mohammadi nasab</h1>')
