4c0a10082b593372beca92898935898c91c72181
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-10 21:46:38 +0430

    Change poorya view to password view

--- Generator/views.py
+++ Generator/views.py
@@ -7,5 +7,5 @@ from django.http import HttpResponse
 def home(request):
     return render(request,'generator/home.html',{'password':'9621160025'})
 
-def Poorya(request):
-    return HttpResponse('<h1>My name is poorya mohammadi nasab</h1>')
+def password(request):
+    return render(request,'generator/password.html')
